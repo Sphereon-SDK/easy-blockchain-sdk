baca43121f0ac441eedf5798c0dcfcd58002473f
Sander <spostma@sphereon.com>
2017-12-15 11:16:07 +0100

    - Added mustache template for okhttp-gson

--- buildAllMavenProfiles.cmd
+++ buildAllMavenProfiles.cmd
@@ -1,7 +1,20 @@
-call mvn -Phtml-docs clean install
-call mvn -Pjava8-okhttp-gson clean install
+cd %~dp0
+del java8-okhttp-gson\pom.xml
+del java8-okhttp-gson\build.gradle
+rmdir java8-okhttp-gson\src\main  /s /q
+del java8-jersey2\pom.xml
+del java8-jersey2\build.gradle
+rmdir java8-jersey2\src\main  /s /q
+del java8-retrofit2\pom.xml
+del java8-retrofit2\build.gradle
+rmdir java8-retrofit2\src\main  /s /q
+rmdir csharp-net35\src\Sphereon.SDK.Blockchain.Proof  /s /q
+rmdir csharp-net45\src\Sphereon.SDK.Blockchain.Proof  /s /q
+
 call mvn -Pjava8-jersey2 clean install
-call mvn -Pjava8-retrofit2 clean install
-call mvn -Pcsharp-net45 clean install
-call mvn -Pcsharp-net35 clean install
-call mvn -Pjavascript clean install
+start mvn -Pjava8-okhttp-gson clean install
+start mvn -Pjava8-retrofit2 clean install
+start mvn -Pcsharp-net45 clean install
+start mvn -Pcsharp-net35 clean install
+start mvn -Pjavascript clean install
+start mvn -Phtml-docs clean install
